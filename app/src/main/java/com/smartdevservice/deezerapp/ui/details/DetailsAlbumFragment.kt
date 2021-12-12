e46f7e5090f38098331164eb95f16466e7972c2a
Ismail BENABDALLAH <benabdallahismail121@gmail.com>
2021-12-12 22:00:05 +0100

    add details album

--- app/src/main/java/com/smartdevservice/deezerapp/ui/details/DetailsAlbumFragment.kt
+++ app/src/main/java/com/smartdevservice/deezerapp/ui/details/DetailsAlbumFragment.kt
@@ -1,14 +1,17 @@
 package com.smartdevservice.deezerapp.ui.details
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.smartdevservice.deezerapp.R
 import com.smartdevservice.deezerapp.base.BaseFragment
 import com.smartdevservice.deezerapp.databinding.FragmentDetailsAlbumBinding
+import com.smartdevservice.deezerapp.utils.EnumDensity
+import com.smartdevservice.deezerapp.utils.Utils
 import com.smartdevservice.deezerapp.utils.Utils.KEY_ALBUM
 import com.smartdevservice.domain.model.Album
 
@@ -45,8 +48,28 @@ class DetailsAlbumFragment : BaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        binding.tvTitle.text = album?.title
+        binding.tvRealise.text = album?.release_date
+        binding.tvArtist.text = album?.artist?.name
+        binding.tvAvailable.text = if(album?.available == true) "Oui" else "Non"
+        binding.tvTracks.text = album?.nb_tracks.toString()
+
+        val enumDensity = Utils.getEnumDensityOfDevice(binding.root.context)
+        val urlCover = getUrlCover(album, enumDensity)
+        Glide.with(binding.root.context)
+            .load(urlCover)
+            .transition(DrawableTransitionOptions.withCrossFade())
+            .placeholder(R.drawable.ic_launcher_background)
+            .into(binding.ivCover)
+    }
+
+    private fun getUrlCover(album: Album?, enumDensity: EnumDensity): String? {
+        return when (enumDensity) {
+            EnumDensity.DENSITY_MDPI -> album?.cover_small
+            EnumDensity.DENSITY_HDPI -> album?.cover
+            EnumDensity.DENSITY_XHDPI -> album?.cover_medium
+            EnumDensity.DENSITY_XXHDPI -> album?.cover_big
+            EnumDensity.DENSITY_XXXHDPI -> album?.cover_xl
         }
     }
 
